0fbe59760c9c53316aba73092f4d27aab237826b
RinaOcean <antyfifa@gmail.com>
2021-12-08 21:18:44 +0200

    mainHeader

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,23 @@
+import styles from './Header.module.css';
+import facultyLogoSvg from '../../imgs/faculty_logo.svg';
+import facultyLogoPng from '../../imgs/faculty_logo.png';
+
+const Header = ()=>{
+  return (
+    <header className={styles.header}>
+    <div className={styles.headerLabel}>
+      
+      <object type="image/svg+xml" data={facultyLogoSvg} width="272" height="110">
+        <img src={facultyLogoPng} width="272" height="110" alt="logo image" />
+      </object>
+      <span>Звіт 2020-2021</span>
+    </div>
+    <div className={styles.missionWrapper}>
+      <h2 className={styles.missionHeading}>Місія</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum ad sequi officiis veniam incidunt quo quam in dolorum laboriosam, ex maiores veritatis quae mollitia a dolor cupiditate doloremque vel qui?</p>
+    </div>
+  </header>
+  )
+}
+
+export default Header;
