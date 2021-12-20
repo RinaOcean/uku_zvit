1dd802470ba7922a361dda87eadacdd8107ae44e
RinaOcean <antyfifa@gmail.com>
2021-12-20 22:41:39 +0200

    Update Chapters.jsx

--- src/components/Chapters/Chapters.jsx
+++ src/components/Chapters/Chapters.jsx
@@ -1,13 +1,15 @@
+import {useNavigate} from "react-router-dom";
 import styles from './Chapters.module.css';
 import Container from '../Container'
 
 const Chapters =()=>{
+  let navigate = useNavigate();
   return (
     <section className={styles.chapters}>
       <Container>
         <ul className={styles.itemsWrapper}>
           <li className={styles.item}><span>Результати вступної кампанії</span></li>
-          <li className={styles.item}><span>Стратегічні цілі факультету 2025</span></li>
+          <li className={styles.item} onClick={() => navigate('/strategichni-cili')}><span>Стратегічні цілі факультету 2025</span></li>
           <li className={styles.item}><span>Структурні зміни</span></li>
           <li className={styles.item}><span>Стдентські перемоги</span></li>
           <li className={styles.item}><span>Успіхи викладачів</span></li>
